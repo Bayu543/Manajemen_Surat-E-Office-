5fe1506a32bb926889372981784fc7c6701f6745
bayuajiyuwono <148197538+Bayu543@users.noreply.github.com>
2026-08-01 12:12:38 +0700

    Update settings.py

--- eoffice/settings.py
+++ eoffice/settings.py
@@ -32,10 +32,14 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*'] # Sebaiknya diganti dengan domain Render di production
 
+# Security settings for reverse proxy
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # CSRF Settings
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:8000',
+    'https://*.up.railway.app',
 ]
 
 
