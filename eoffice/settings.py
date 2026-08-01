55922dced3ce92337f0092dc4c759d92f7cd6ba9
Bayu aji <yuwonoajibayu@gmail.com>
2026-08-01 13:40:09 +0700

    Fix CSRF for mobile

--- eoffice/settings.py
+++ eoffice/settings.py
@@ -40,6 +40,8 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:8000',
     'https://*.up.railway.app',
+    'https://manajemensurat-e-office-production.up.railway.app',
+    'http://manajemensurat-e-office-production.up.railway.app',
 ]
 
 
